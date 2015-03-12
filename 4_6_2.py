18b04b9424c4784c9af7a2e753e711983adea433
juclark96 <jcc2244@barnard.edu>
2015-03-12 23:47:16 +0000

    new

--- 4_6_2.py
+++ 4_6_2.py
@@ -1,5 +1,7 @@
-for i in range(1,1000):
-    for j in range(3,8):
-        product = i/3
-        print product
-    print ""
+divisible = []
+
+for i in range(0,1000):
+    if (i%21 == 0):
+        divisible.append(i)
+        
+print divisible
---
+++ 4_6_3.py
@@ -0,0 +1,6 @@
+from random import random as rand
+x = []
+for i in range(10):
+    x.append( rand() * 10 )
+print x
+
